001491cafa9e24db860cd33cfca38bd2dc6c5806
yuhazakaria <yuhazakaria@gmail.com>
2019-07-16 17:08:37 +0700

    latihan array

---
+++ latihan array/src/com/tutorial/Main.java
@@ -0,0 +1,11 @@
+package com.tutorial;
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main (String[] args){
+        // assignment
+        // tipedata [] nama = {komponen-komponen}
+
+    }
