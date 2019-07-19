789693bb9d5846ca9646e6ae9b72013fbd6bd243
yuhazakaria <yuhazakaria@gmail.com>
2019-07-19 16:43:24 +0700

    otak-atik

--- latihan array/src/com/tutorial/Main.java
+++ latihan array/src/com/tutorial/Main.java
@@ -1,11 +1,39 @@
 package com.tutorial;
 
-import java.util.Arrays;
-
 public class Main {
 
-    public static void main (String[] args){
-        // assignment
-        // tipedata [] nama = {komponen-komponen}
+    public static void main(String[] args) {
+    
+        int value1 = 5;
+        int value2 = 4;
+        boolean hasil;
+		hasil = value1 == value2;
 
-    }
+        System.out.println("Hasil 'value1 == value2' adalah " + hasil);
+        System.out.println();
+        System.out.println("tidak sama dengan..");
+
+        hasil = value1 != value2;
+
+        System.out.println("hasil 'value1 != value2' adalah " + hasil);
+        System.out.println();
+        System.out.println("lebih besar dari..");
+
+        hasil = value1 > value2;
+
+        System.out.println("Hasil 'value1 > value2' adalah " + hasil);
+        System.out.println();
+        System.out.println("sama atau lebih besar dari..");
+
+        hasil = value1 >= value2;
+
+        System.out.println("hasil 'value >= value2' adalah " + hasil);
+        System.out.println();
+        System.out.println("sama atau kurang dari dengan..");
+
+        hasil = value1 <= value2;
+
+        System.out.println("hasil 'value1 <= value2' adalah " + hasil);
+        System.out.println();
+        }
+}
